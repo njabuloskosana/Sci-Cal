b1a0b7922acadc6417646626e64c6f5eb051f5f6
Njabulo <njabulo@xcontent.com>
2022-03-19 09:02:07 +0200

    Arithmetic practical code setup

---
+++ scientific-calculator/src/ArithmeticOperators.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+
+public class ArithmeticOperators {
+
+    Scanner scan = new Scanner(System.in); 
+
+    public void showMenu() {
+		System.out.println("1.Addition");
+		System.out.println("2.Substraction");
+		System.out.println("3.Multiplication");
+		System.out.println("4.Division");
+
+	}
+
+    //Add comments
+    public double division(double firstNumber, double secondNumber) {
+		//Add your code
+		return 0.0;
+	}
+
+    //Add comments
+	public double mutliplication(double firstNumber, double secondNumber) {
+		//Add your code
+		return 0.0;
+	}
+
+    //Add comments
+	public double substraction(double firstNumber, double secondNumber) {
+		//Add your code
+		return 0.0;
+	}
+
+    /**
+     * This function performs addition on two numbers and returns the result of the addition operation
+     * @param firstNumber
+     * @param secondNumber
+     * @return
+     */
+	public double addition(double firstNumber, double secondNumber) {
+		double val = firstNumber + secondNumber;
+		return val;
+	}
+
+
+    
+}
