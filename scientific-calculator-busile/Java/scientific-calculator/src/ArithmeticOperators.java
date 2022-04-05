904cbd5076edadc14ab79c0e61671091b556b1fc
Njabulo <njabulo@xcontent.com>
2022-04-05 17:02:54 +0200

    Setting up busiles assignment environment in java

---
+++ scientific-calculator-busile/Java/scientific-calculator/src/ArithmeticOperators.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+
+public class ArithmeticOperators {
+
+    Scanner scan = new Scanner(System.in); 
+
+    public void showMenu() {
+		System.out.println("1.Addition");
+		System.out.println("2.Subtraction");
+		System.out.println("3.Multiplication");
+		System.out.println("4.Division");
+	}
+
+    //Add comment
+    public double division(double firstNumber, double secondNumber) {
+		//Add your code
+		return 0.0;
+	}
+
+    //Add comment
+	public double mutliplication(double firstNumber, double secondNumber) {
+		//Add your code
+		return 0.0;
+	}
+
+    //Add comment
+	public double substraction(double firstNumber, double secondNumber) {
+		//Add your code
+		return 0.0;
+	}
+
+    //Add comment
+	public double addition(double firstNumber, double secondNumber) {
+		//Add your code
+		return 0.0;
+	}
+
+	//Add comment
+	public double modulus(double firstNumber, double secondNumber)
+	{
+		//Add code
+		return 0.0;
+	}
+
+
+	//Add comment
+	public double modulusComplex(double firstNumber, double secondNumber, double thirdNumber)
+	{
+		//Add code
+		return 0.0;
+	}
+	
+		
+
+
+
+}
+
---
+++ scientific-calculator-busile/Java/scientific-calculator/src/Calculator.java
@@ -0,0 +1,63 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+ /**
+  * Very very simple Java Scientific Calculator for beginners
+ */
+
+public class Calculator {
+	
+	Scanner scan = new Scanner(System.in); 
+	public static void main(String[] args) {
+		ArithmeticOperators calc = new ArithmeticOperators();
+		Scanner scan = new Scanner(System.in);
+		scan.useLocale(Locale.US);
+		int x;
+		double score;
+		calc.showMenu();
+		x = scan.nextInt();
+
+		switch (x) {
+		case 1:
+            double a, b;
+            System.out.println("Enter first value");
+	    	a = scan.nextDouble();
+	    	System.out.println("Enter first value");
+		    b = scan.nextDouble();
+			score = calc.addition(a,b);
+			System.out.println("Your result is : "+ score);
+            scan.close();
+			break;
+
+		case 2:
+			//Add your code
+			scan.close();
+			break;
+
+		case 3:
+			//Add your code
+			scan.close();
+			break;
+
+		case 4:
+			//Add your code
+			scan.close();
+			break;
+
+		case 5:
+			//Add your code
+			scan.close();
+			break;
+		case 6:
+			//Add your code
+			scan.close();
+			break;
+		
+
+		default:
+			System.out.println("Wrong choice");
+			break;
+		}
+	}
+
+}
